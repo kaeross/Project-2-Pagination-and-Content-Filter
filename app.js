4e5fc6f4a4395d02b6dff19ef109c0f6e688e8d1
kaeross <kaeross@icloud.com>
2017-06-07 15:45:01 +0100

    Pagination added
    write page function added to add pagination to bottom of student list

--- app.js
+++ app.js
@@ -3,45 +3,53 @@ const studentName = $('.student-item h3');
 const studentEmail = $('.email');
 const studentJoined = $('.date');
 const studentArray = [];
+let pageNum = 1;
 const page = [];
 let studentsPerPage = 10;
 const numberOfPages = Math.ceil(studentList.length / studentsPerPage - 1);
+const pagination = $('.pagination');
 
 //on page load - hide all but first 10 students
-$(document).ready(showPage(page, studentArray));
+$(document).ready(showPage(0));
 
 //function to display 10 students according to which page number the student selected
-function showPage() {
-	studentList.hide();
+function showPage(pageNum) {
+		studentList.hide();
 
-	
+	//assign students to pages
 	studentArray.push(studentList.splice(0, 10));
-
 	for (let i = 0; i < numberOfPages; i+=1) {
 		page[i] = studentArray.push(studentList.splice(0, 10));
 	}
+//on load show first 10 students
+	$.each(studentArray[pageNum], function() {
+		$(this).show().addClass('onPage');
+	});
+
 
 //on click hide 1st 10 students when button 2 selected and show 2nd group
 
-	$.each(studentArray[1], function() {
-		$(this).show();
-	});
+
 }
 
-
+pagination.on("click", function() {
+	if (studentList.hasClass('onPage')) {
+		$(this).hide();}
+	$(document).showPage(4);
+});
 
 function writePage() {
-	for (let i = 0; i < numberOfPages.length; i+=1) {
-		let pageNum = page[i];
-		let pageListNum ='<li><a class="active" href="#">';
-		pageListNum += 1;
-		pageListNum += '</a></li>';
-		page += 1;
-		return pageListNum;
+	let paginationHTML = '<div class="pagination"><ul>';
+	for (let i = 0; i <= numberOfPages; i+=1) {
+		paginationHTML +='<li><a class="active" href="page_' + pageNum + '">' + pageNum + '</a></li>';
+		pageNum += 1;
 	}
-
+	paginationHTML += '</ul></div>';
+	return paginationHTML;
 }
 
+
+// page x studentArray[x-1];
 
 
 // function appendPageLinks() {
@@ -50,7 +58,7 @@ function writePage() {
 // 		$(this).append('<div class="pagination"><Page</div>');
 // };
 
-$('.student-list').append('<div class="pagination"><ul>' + 1 + '</ul></div>');
+$('.student-list').append(writePage());
 
 //add pagination with following markup
       // <div class="pagination">
