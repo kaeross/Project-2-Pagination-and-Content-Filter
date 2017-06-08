460afb7f0eee0f24d238011813bb5f0adeeb1433
kaeross <kaeross@icloud.com>
2017-06-08 14:05:04 +0100

    Refining
    Neatened code and added active class to page 1 on load

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const studentArray = [];
 let pageNum = 1;
 let studentsPerPage = 10;
 const numberOfPages = Math.ceil(studentList.length / studentsPerPage - 1);
-const pagination = $('.pagination');
 
 function showList() {
 	$(this).show();
@@ -16,53 +15,43 @@ function hideList() {
 	$(this).hide();
 } 
 
-let paginationNumber = pagination.find('a');
-
 //on page load - hide all but first 10 students
 $(document).ready(showPage(0));
 
-//function to display 10 students according to which page number the student selected
+//Assign Students to pages and show according to page
 function showPage(pageNum) {
 	studentList.hide();
-
-	//assign students to pages
 	if (studentList.length != []) {
 		studentArray.push(studentList.splice(0, 10));
 		for (let i = 0; i < numberOfPages; i+=1) {
 			studentArray.push(studentList.splice(0, 10));
 		}
 	}
-	//show 10 students
 	$.each(studentArray[pageNum], showList);
-
-	//reset student list
 	studentList = $('.student-item');
 }            
 
 
-
+//Create Pagination and append to student list
 function writePage() {
 	let paginationHTML = '<div class="pagination"><ul>';
 	for (let i = 0; i <= numberOfPages; i+=1) {
 		paginationHTML +='<li><a class="" href="#' + pageNum.toString() + '">' + pageNum.toString() + '</a></li>';
-
-		//if active addClass active 
-
 		pageNum += 1;
 	}
 	paginationHTML += '</ul></div>';
 	return paginationHTML;
 }
 $('.student-list').append(writePage());
+$('.pagination a').first('a').addClass('active');
 
 
 
-//When a user clicks on “2” in the pagination, students 11 through 20 are shown. When a user clicks “3”, students 21 through 30 are shown.
+//Pagination conected to "pages"
 const links = $('a');
 links.on('click', function(e){
 	let numButton = $(e.target).text();
 	parseInt(numButton);
-
 	if(links.siblings('.active')) {
 		links.removeClass('active');
 	}
